f2d89fbc481ace0b562254b968798e2815dbe607
User <Kozheletovan@mail.ru>
2022-10-18 16:22:38 +0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+     val element = listOf<String>()
+for (i in 0..11){
+    println("element")
+}
+}
---
+++ src/main/kotlin/option2.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val number = listOf<Int>()
+
+    for (i in 0..20){
+        number=
+    println(number)
+}
